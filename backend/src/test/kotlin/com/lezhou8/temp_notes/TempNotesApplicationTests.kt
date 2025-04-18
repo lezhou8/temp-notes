6d1f054a59630763a58f3741bd07d5abf0ae288b
lezhou8 <dll145@protonmail.com>
2025-04-18 12:16:11 +1000

    testing framework established

--- backend/src/test/kotlin/com/lezhou8/temp_notes/TempNotesApplicationTests.kt
+++ backend/src/test/kotlin/com/lezhou8/temp_notes/TempNotesApplicationTests.kt
@@ -1,13 +1,17 @@
 package com.lezhou8.temp_notes
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.web.reactive.server.WebTestClient
 
-@SpringBootTest
-class TempNotesApplicationTests {
+@AutoConfigureWebTestClient
+@Import(PostgresTestcontainersConfiguration::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TempNotesApplicationTests(@Autowired var webTestClient: WebTestClient) {
 
 	@Test
-	fun contextLoads() {
-	}
-
+	fun contextLoads() {}
 }
